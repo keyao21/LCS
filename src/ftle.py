10533a15efa620e5e28490121e040400cc63ba73
keyao21 <k.yyao7@gmail.com>
2018-02-09 09:49:34 -0500

    plotter

--- src/ftle.py
+++ src/ftle.py
@@ -13,6 +13,7 @@ class FTLE():
         self.state = np.zeros((self.length,4))
         self.dt = 0.1
         self.elapsedTime = 20
+        self.mappingfile = None
 
     def velocity(self, x, y, t, a = 0.1, epsilon = 0.25, w = 0.2*np.pi, dims = 2):
         f = epsilon*np.sin(w*t)*x**2 + (1-2*epsilon*np.sin(w*t))*x
@@ -50,13 +51,18 @@ class FTLE():
         #state[:,4:6] += noise
         return state
 
-    def createMapping(self, dt=None, elapsedTime=None):
+    def createMapping(self, mappingfile=None, dt=None, elapsedTime=None):
         if dt is None:
             dt = self.dt
         if elapsedTime is None:
             elapsedTime = self.elapsedTime
+        if mappingfile is None:
+            mappingfile = self.mappingfile
+        else:
+            self.mappingfile = mappingfile
 
-        output = open('data/kevin_output_steps.txt','ab')
+        # output = open('data/kevin_output_steps.txt','ab')
+        output = open(mappingfile,'ab')
 
         for i in np.linspace(0.01, 0.99, self.height, np.float64):
             self.state = np.zeros((self.length, 2))  # (xposition, yposition)
@@ -68,10 +74,55 @@ class FTLE():
                 
             np.savetxt(output,self.state)
 
+    def plotter(self):
+ 
+        def Jacobian(X,Y):
+            dx = 1.98/(self.length-1) # horizontal grid difference
+            dy = 0.98/(self.height-1) # verticle grid difference
+            J = np.empty([2,2],float)
+            FTLE = np.empty([H-2,L-2],float)
+            
+            for i in range(0,H-2):
+                for j in range(0,L-2):
+                    J[0][0] = (X[(1+i)*L+2+j]-X[(1+i)*L+j])/(2*dx)
+                    J[0][1] = (X[(2+i)*L+1+j]-X[i*L+1+j])/(2*dx)
+                    J[1][0] = (Y[(1+i)*L+2+j]-Y[(1+i)*L+j])/(2*dy)
+                    J[1][1] = (Y[(2+i)*L+1+j]-Y[i*L+1+j])/(2*dy)
+                    
+                    D = np.dot(np.transpose(J),J) # Green-Cauchy tensor
+                    lamda = LA.eigvals(D) # its largest eigenvalue
+                    FTLE[i][j] = max(lamda)
+            return FTLE
+
+        mappingfile = open(self.mappingfile,'r')
+        X,Y = np.loadtxt(mappingfile,unpack = True)
+        mappingfile.close()
+
+        # scatter plot
+        plt.figure(1)
+        plt.scatter(X,Y)
+
+        # FTLE plot
+        plt.figure(2)
+        FTLE = Jacobian(X,Y)
+        FTLE = np.log(FTLE)
+
+        ax = plt.subplot(111)
+        im = ax.imshow(FTLE)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right",size = "5%",pad = 0.1)
+        plt.colorbar(im, cax = cax)
+        #plt.gca().invert_yaxis()
+
+        print(time.time()-start_time)
+        plt.show()
+
+
 def main():
+    datafolder = 'data'
+    mappingfile = datafolder + '/kevin_output_steps.txt'
     test = FTLE()
-    test.createMapping()
-    test.state
+    test.createMapping(mappingfile)
 
 if __name__ == '__main__':
     main()
