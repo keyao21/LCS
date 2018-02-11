14dadb7d217b75672f4b9f835ba23c26bacdf61b
keyao21 <k.yyao7@gmail.com>
2018-02-10 23:27:48 -0500

    fixed bilinear interp

--- src/ftle.py
+++ src/ftle.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy import interpolate
+from scipy.interpolate import RegularGridInterpolator
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 import time as time
+import pdb
 
 class FTLE():
     def __init__(self, **kwargs):
@@ -13,8 +15,8 @@ class FTLE():
         self.percentHeight = 1
         self.minx = 0.01
         self.miny = 0.01
-        self.maxx = 1.99
-        self.maxy = 0.99
+        self.maxx = 2
+        self.maxy = 1
         self.delta = 1e-5
         self.earthRotation = 1
         self.phi = np.pi*40.7128/180  # lattitude
@@ -51,7 +53,6 @@ class FTLE():
         elif dims == 3:
             print( "not sure if we can do that yet!")
         # return np.vstack([vx,vy])
-        print(_vx.shape)
         return _vx, _vy
 
     # def getEstimatedVelocity(self, x, y, t):
@@ -64,18 +65,36 @@ class FTLE():
 
     #     return self.doubleGyreVelocity(est_x, est_y, t)
 
+    # def getEstimatedVelocity(self, x, y, t):
+    #     _xarr = np.linspace(self.minx, self.maxx, self.length*self.percentLength)
+    #     _yarr = np.linspace(self.miny, self.maxy, self.height*self.percentHeight)
+    #     xx_, yy_ = np.meshgrid(_xarr,_yarr)
+    #     vx_, vy_ = self.doubleGyreVelocity(xx_, yy_, t)[0], self.doubleGyreVelocity(xx_, yy_, t)[1] 
+    #     vxfunc, vyfunc = interpolate.interp2d(_xarr, _yarr, vx_), interpolate.interp2d(_xarr, _yarr, vy_)
+    #     vx, vy = vxfunc(x, y)[0], vyfunc(x, y)[0]
+    #     # print(vx)
+    #     return vx, vy
+
+
     def getEstimatedVelocity(self, x, y, t):
+        # pdb.set_trace()
         _xarr = np.linspace(self.minx, self.maxx, self.length*self.percentLength)
         _yarr = np.linspace(self.miny, self.maxy, self.height*self.percentHeight)
-        xx_, yy_ = np.meshgrid(_xarr,_yarr)
-        vx_, vy_ = self.doubleGyreVelocity(xx_, yy_, t)[0], self.doubleGyreVelocity(xx_, yy_, t)[1] 
-        vxfunc, vyfunc = interpolate.interp2d(_xarr, _yarr, vx_), interpolate.interp2d(_xarr, _yarr, vy_)
-        vx, vy = vxfunc(x, y), vyfunc(x, y)
-        print(vx.shape)
-        return vx, vy
+        xdata = self.doubleGyreVelocity(*np.meshgrid(_xarr, _yarr, indexing='ij', sparse=True), t)[0]
+        ydata = self.doubleGyreVelocity(*np.meshgrid(_xarr, _yarr, indexing='ij', sparse=True), t)[1]
+        # vxfunc,vyfunc = interpolate.interp2d(_xarr, _yarr, xdata), interpolate.interp2d(_xarr, _yarr, ydata)
+        vxfunc = RegularGridInterpolator((_xarr, _yarr), xdata, method='nearest')
+        vyfunc = RegularGridInterpolator((_xarr, _yarr), ydata, method='nearest')
+        x, y = np.clip(x,self.minx,self.maxx), np.clip(y,self.miny,self.maxy)
+        points = np.transpose(np.array([x, y]))
+        outxv, outxy = vxfunc(points), vyfunc(points)
+        return outxv, outxy
+
 
     def getVelocity(self, x, y, t):
+        # print(self.doubleGyreVelocity(x, y, t)[0])
         return self.doubleGyreVelocity(x, y, t)
+
 
     def update(self, state, t):
         x = state[:,0]
@@ -98,14 +117,15 @@ class FTLE():
         k3 = dt*self.update(tmp_state+0.5*k2,t+0.5*dt)
         k4 = dt*self.update(tmp_state+k3,t+dt)
         tmp_state += (k1+2*k2+2*k3+k4)/6
-        state[:,0] = np.clip(tmp_state[:,0],0.00001,1.99999)
-        state[:,1] = np.clip(tmp_state[:,1],0.00001,0.99999)
+        state[:,0] = np.clip(tmp_state[:,0],self.minx,self.maxx)
+        state[:,1] = np.clip(tmp_state[:,1],self.miny,self.maxy)
 
         #noise = B*np.random.normal(u,sigma,(L,2))
         #state[:,4:6] += noise
         return state
 
     def createMapping(self, mappingfile=None, dt=None, elapsedTime=None):
+        # pdb.set_trace()
         if dt is None:
             dt = self.dt
         if elapsedTime is None:
@@ -148,7 +168,6 @@ class FTLE():
             return FTLE
 
         start_time = time.time()
-
         mappingfile = open(self.mappingfile,'r')
         X,Y = np.loadtxt(mappingfile,unpack = True)
         mappingfile.close()
@@ -182,12 +201,19 @@ class FTLE():
         if display:
             plt.show()
 
+    def testVelocityDiff(self):
+        x = np.random.rand(100)
+        y = np.random.rand(100)
+        print( np.mean(np.subtract(self.getVelocity(x,y,20), self.getEstimatedVelocity(x,y,20))) ) 
 
 def main():
+
     datafolder = '../data'
     mappingfile = datafolder + '/kevin_output_steps.txt'
-    test = FTLE(length=200)
+    test = FTLE(length=100, height=50, percentLength=0.1, percentHeight=0.1, estimateVelocity=True)
     # test.createMapping(mappingfile)
+    # test.plotter(display=True, title='test', filename='TESTS')
+    test.testVelocityDiff()
 
 if __name__ == '__main__':
     main()
